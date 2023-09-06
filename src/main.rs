a1256eca2723129ad14ebb0a2dea8ba8e5f21441
kpzip <kpzipgames@gmail.com>
2023-09-05 19:20:36 -0700

    basic web server working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::fs;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    //Main event loop
+    for stream in listener.incoming() {
+        let stream: TcpStream = stream.unwrap();
+
+        //println!("Connection established!");
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader: BufReader<&mut TcpStream> = BufReader::new(&mut stream);
+    let http_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let request_type: &String = http_request.first().unwrap();
+
+    let (status, filename): (&str, &str) = match &request_type[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents: String = fs::read_to_string(filename).unwrap();
+    let length: usize = contents.len();
+
+    let response: String = format!("{status}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+
+    //println!("Request: {:#?}", http_request);
 }
