4e001a8011935f0b2edf115198bfcc03759c89d5
kpzip <kpzipgames@gmail.com>
2023-10-09 22:06:20 -0700

    use wrlock

--- src/resourcemanager.rs
+++ src/resourcemanager.rs
@@ -1,19 +1,33 @@
+use std::mem::drop;
+use std::sync::RwLock;
 use std::{fs, io::Error};
 
-static mut RESOURCES: Vec<CachedResource> = vec![];
+static RESOURCES: RwLock<Vec<CachedResource>> = RwLock::new(Vec::new());
 
-pub fn get_resource_data(name: &str) -> Result<String, Error> {
-    unsafe { if RESOURCES.iter().any(|s| s.name == name) {
-
-    } }
-
-    fs::read_to_string(name)
+pub fn get_resource_data(name: &str) -> &'static String {
+    let reader = RESOURCES.read().expect("Poisoned Resource Manager!").iter();
+    for resource in reader {
+        if *resource.name == *name {
+            return &resource.data;
+        }
+    }
+    let filedata: String = match fs::read_to_string(name) {
+        Ok(s) => s,
+        Err(_) => return &String::new(),
+    };
+    let writer = RESOURCES.write().expect("Poisoned Resource Manager!");
+    writer.push(CachedResource::new(name, filedata));
+    drop(writer);
+    return &filedata;
 }
-
-unsafe fn get_cached_resource
-
 
 struct CachedResource<'a> {
     data: String,
     name: &'a str,
+}
+
+impl CachedResource<'_> {
+    fn new(name: &str, data: String) -> CachedResource {
+        CachedResource { data, name }
+    }
 }
